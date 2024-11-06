705f75bbc4a36462947b229a8a05665c20a581de
Nick <niickfarahmand@gmail.com>
2024-11-06 16:29:41 -0500

    first commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: `Nick's Store`,
+  title: `New Gen of Online Store`,
+  description:
+    'Hotkicks Ecommerce Template in NextJS, React, HTML and TailwindCSS',
+  locale: 'en',
+};
